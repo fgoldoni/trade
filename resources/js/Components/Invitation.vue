1d60a8ce0cfe34605a7198554bbc87336e728603
Goldoni <fotsa.goldoni@yahoo.fr>
2024-12-11 07:09:23 +0100

    initial commit

--- resources/js/Components/Invitation.vue
+++ resources/js/Components/Invitation.vue
@@ -39,7 +39,7 @@ onMounted(async () => {
             <div class="relative">
                 <h2 class="relative text-xl font-black text-transparent bg-center bg-cover bg-gradient-to-br from-indigo-400 via-indigo-600 to-indigo-500 lg:text-2xl bg-clip-text" style="background-image:url('https://cdn.devdojo.com/images/february2021/bg-colorful.jpg')">Lien d'invitation</h2>
             </div>
-            <p class="mt-3 text-xl text-gray-500 lg:text-2xl">Code: {{ $page.props.auth.user.referral_code }}</p>
+            <p class="mt-3 text-sm text-gray-500 lg:text-xl">Code: {{ $page.props.auth.user.referral_code }}</p>
             <div class="flex flex-col justify-center w-full mt-5 space-y-3 sm:space-x-3 sm:space-y-0 sm:flex-row lg:mt-8">
                 <div class="w-full px-8">
                     <div class="relative">
@@ -50,13 +50,13 @@ onMounted(async () => {
                 <svg class="w-3 h-3 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                     <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"/>
                 </svg>
-                <span class="text-xs font-semibold">Copy</span>
+                <span class="text-xs font-semibold">Copier</span>
             </span>
                             <span id="success-message" class="hidden inline-flex items-center">
                 <svg class="w-3 h-3 text-blue-700 dark:text-blue-500 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
                 </svg>
-                <span class="text-xs font-semibold text-blue-700 dark:text-blue-500">Copied</span>
+                <span class="text-xs font-semibold text-blue-700 dark:text-blue-500">Copié</span>
             </span>
                         </button>
                     </div>
