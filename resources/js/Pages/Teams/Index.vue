904a65cc1560ced715dac34f8c9ca2ffef0886a3
Goldoni <fotsa.goldoni@yahoo.fr>
2024-12-16 12:47:15 +0100

    initial commit

--- resources/js/Pages/Teams/Index.vue
+++ resources/js/Pages/Teams/Index.vue
@@ -6,6 +6,8 @@ import ProductsComponent from "@/Pages/Products/ProductsComponent.vue";
 import Heading from "@/Pages/Deposits/Heading.vue";
 import HeadingReferralComponent from "@/Pages/Teams/HeadingReferralComponent.vue";
 import ReferralComponent from "@/Pages/Teams/ReferralComponent.vue";
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/vue/20/solid'
+
 
 defineProps<{
     total?: Number;
@@ -18,8 +20,27 @@ defineProps<{
     <Head title="Welcome" />
     <GuestLayout>
         <div class="p-8">
-            <HeadingReferralComponent title="Équipe" :total="total"></HeadingReferralComponent>
-            <ReferralComponent title="Équipe" :total="total"></ReferralComponent>
+            <HeadingReferralComponent title="Équipe" :total="revenue"></HeadingReferralComponent>
+            <div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+                <div class="-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
+                    <div class="ml-4 mt-4">
+                        <div class="flex items-center">
+                            <div>
+                                <h3 class="text-base font-semibold text-gray-900">Mon Équipe</h3>
+                                <p class="text-sm text-gray-500">
+                                    <a href="#">{{ referrals.length }} Membres</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ml-4 mt-4 flex shrink-0">
+                        <button type="button" class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                            <span>Total: {{ total }} {{ $page.props.app.currency }}</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <ReferralComponent title="Équipe" :referrals="referrals"></ReferralComponent>
         </div>
     </GuestLayout>
 </template>
--- resources/js/Pages/Teams/ReferralComponent.vue
+++ resources/js/Pages/Teams/ReferralComponent.vue
@@ -1,28 +1,25 @@
 <template>
-    <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <li v-for="person in people" :key="person.email" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+    <ul role="list" class="py-8 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <li v-for="person in referrals" :key="person.id" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-slate-300 shadow">
             <div class="flex w-full items-center justify-between space-x-6 p-6">
                 <div class="flex-1 truncate">
-                    <div class="flex items-center space-x-3">
-                        <h3 class="truncate text-sm font-medium text-gray-900">{{ person.name }}</h3>
-                        <span class="inline-flex shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{{ person.role }}</span>
+                    <div class="flex items-center space-x-3 mb-2" v-for="product in person.products" :key="product.id">
+                        <h3 class="truncate text-sm font-medium text-gray-900">{{ product.name }}</h3>
+                        <span class="inline-flex shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{{ product.formatted_price }} {{ $page.props.app.currency }}</span>
                     </div>
-                    <p class="mt-1 truncate text-sm text-gray-500">{{ person.title }}</p>
                 </div>
-                <img class="size-10 shrink-0 rounded-full bg-gray-300" :src="person.imageUrl" alt="" />
+                <img class="size-10 shrink-0 rounded-full bg-gray-300" src="/images/awards/award-1.svg" alt="" />
             </div>
             <div>
                 <div class="-mt-px flex divide-x divide-gray-200">
                     <div class="flex w-0 flex-1">
-                        <a :href="`mailto:${person.email}`" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
-                            <EnvelopeIcon class="size-5 text-gray-400" aria-hidden="true" />
-                            Email
+                        <a href="javascript:;" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                            {{ person.products.length }} Pack(s)
                         </a>
                     </div>
                     <div class="-ml-px flex w-0 flex-1">
-                        <a :href="`tel:${person.telephone}`" class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
-                            <PhoneIcon class="size-5 text-gray-400" aria-hidden="true" />
-                            Call
+                        <a href="javascript:;" class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                            {{ person.name }}
                         </a>
                     </div>
                 </div>
@@ -31,8 +28,12 @@
     </ul>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { EnvelopeIcon, PhoneIcon } from '@heroicons/vue/20/solid'
+
+defineProps<{
+    referrals?: Object;
+}>();
 
 const people = [
     {
